b9406fc673323cb33e4b2d3ca54df3166019989f
Andrew Peterkort <andrew.j.peterkort@gmail.com>
2018-06-21 12:04:20 -0700

    Got a basic two way attack going with a basic AI that just picks the best attack for each. Random numbers seem to give 9 way too often... could be glitch.

--- HighschoolRPG/Program.cs
+++ HighschoolRPG/Program.cs
@@ -4,13 +4,14 @@ namespace HighschoolRPG
 {
     class Program
     {
-
         static void Main()
         {
+            Random rnd = new Random();
+            Student newStudentOne = new Student( "AI_One", rnd.Next(0, 10), rnd.Next(0, 10), rnd.Next(0, 10), rnd.Next(0, 10));
+            Student newStudentTwo = new Student("AI_Two", rnd.Next(0, 10), rnd.Next(0, 10), rnd.Next(0, 10), rnd.Next(0, 10));
 
-            Student newStudent = new Student();
-            newStudent.functionOne();
-            Console.WriteLine("BLAH");
+            newStudentTwo.defend(newStudentOne.attack());
+            newStudentOne.defend(newStudentTwo.attack());
 
         }
     }
--- HighschoolRPG/Student.cs
+++ HighschoolRPG/Student.cs
@@ -1,11 +1,93 @@
 ﻿using System;
 namespace HighschoolRPG
 {
+    public struct Stats
+    {
+        public int Looks;
+        public int Wits;
+        public int Smarts;
+        public int Style;
+    }
+
+    public struct Attack
+    {
+        public int damage;
+        public int stat;
+    }
+
     public class Student
     {
-        public void functionOne()
+        public string Name;
+        public Stats allStats;
+        public int Hp;
+        public int Losses;
+
+        private int getHp()
         {
-            Console.WriteLine("THIS IS THE SECOND CLASS");
+            return allStats.Looks + allStats.Wits + allStats.Smarts + allStats.Style + Losses;
+        }
+
+        public Attack attack()
+        {
+            int current;
+            int countHigh = 0;
+            int high = 0;
+            int count = 0;
+
+            Attack newAt;
+
+            foreach( var stat in typeof(Stats).GetFields( System.Reflection.BindingFlags.Public | System.Reflection.BindingFlags.Instance) )
+            {
+                count++;
+                current = Convert.ToInt32(stat.GetValue(allStats));
+                if ( current > high )
+                {
+                    high = current;
+                    countHigh = count;
+                }
+            }
+
+            newAt.damage = high;
+            newAt.stat = countHigh;
+            Console.WriteLine("NEW ATTACK");
+            Console.WriteLine("HIGHEST NUM: {0}", high);
+            Console.WriteLine("NUM POSITION: {0}", countHigh);
+            return newAt;
+        }
+
+        public void defend(Attack input)
+        {
+            int count = 0;
+            int current = 0;
+            int damage = 0;
+
+            foreach (var stat in typeof(Stats).GetFields(System.Reflection.BindingFlags.Public | System.Reflection.BindingFlags.Instance))
+            {
+                count++;
+                current = Convert.ToInt32(stat.GetValue(allStats));
+                if (count == input.stat)
+                {
+                    damage = input.damage - current;
+                    
+                }
+
+            }
+
+            damage = (damage > 0) ? damage : 0;
+            Hp -= damage;
+            Console.WriteLine("{0} toke {1} points of damage and is now at {2} HP", Name, damage, Hp);
+
+        }
+
+        public Student( string name, int looks, int wits, int smarts, int style )
+        {
+            this.Name = name;
+            this.allStats.Looks = looks;
+            this.allStats.Wits = wits;
+            this.allStats.Smarts = smarts;
+            this.allStats.Style = style;
+            this.Hp = getHp();
+            this.Losses = 0;
         }
     }
 }
